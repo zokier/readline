00728af722ae793957b3c0d275008c0e51e49da5
Nicholas Mazzuca <npmazzuca@gmail.com>
2015-01-03 22:11:03 -0800

    [breaking] Read the extended message
    * Broke readline-sys out into its own repo
    * deleted the build.rs leftover
    * Broke add_history() out into its own command
    * update to v0.0.3

--- readline-sys/lib.rs
+++
@@ -1,7 +0,0 @@
-#[cfg(any(target_os = "linux", target_os = "macos"))]
-extern crate libc;
-use self::libc::c_char;
-extern "C" {
-    pub fn readline(prompt: *const c_char) -> *mut c_char;
-    pub fn add_history(line: *const c_char);
-}
--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,9 @@ use std::io::IoResult;
 
 use std::c_str::ToCStr;
 
-#[cfg(any(target_os = "linux", target_os = "macos"))]
 pub fn readline(prompt: &str) -> IoResult<String> {
     use std::io::{IoError, IoErrorKind};
-    return unsafe {
+    unsafe {
         // It doesn't matter if there is an interior null
         // It just won't prompt all the way 
         let line_ptr = 
@@ -20,7 +19,6 @@ pub fn readline(prompt: &str) -> IoResult<String> {
             });
         }
 
-        ffi::add_history(line_ptr as *const i8);
 
         let line = String::from_raw_buf(line_ptr as *const u8);
 
@@ -28,19 +26,6 @@ pub fn readline(prompt: &str) -> IoResult<String> {
     }
 }
 
-
-#[cfg(not(any(target_os = "linux", target_os = "macos")))]
-pub fn readline(prompt: &str) -> IoResult<String> {
-    use std::io::stdio::stdin;
-    print!("{}", prompt);
-    let line = stdin().read_line();
-    match line {
-        Ok(mut s) => {
-            s.pop(); // takes the last \n off of the returned string
-            Ok(s)
-        },
-        Err(e) => {
-            Err(e)
-        },
-    }
+pub fn add_history(line: &str) {
+    unsafe { ffi::add_history(line.as_ptr() as *const i8) };
 }
