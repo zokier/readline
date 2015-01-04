da99a057bc1832072d8794db8f6fc99ba9b552fd
Nicholas Mazzuca <npmazzuca@gmail.com>
2015-01-03 20:53:22 -0800

    [BREAKING] Switch to IoResult from Option

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 extern crate "readline-sys" as ffi;
+use std::io::IoResult;
 
 #[cfg(any(target_os = "linux", target_os = "macos"))]
-pub fn readline(prompt: &str) -> Option<String> {
+pub fn readline(prompt: &str) -> IoResult<String> {
+    use std::io::{IoError, IoErrorKind};
     return unsafe {
         // It doesn't matter if there is an interior null
         // It just won't prompt all the way 
@@ -9,35 +11,34 @@ pub fn readline(prompt: &str) -> Option<String> {
             ffi::readline(prompt.to_c_str_unchecked().as_ptr());
 
         if line_ptr.is_null() {
-            return None;
+            return Err(IoError { 
+                kind: IoErrorKind::EndOfFile,
+                desc: "end of file",
+                detail: None,
+            });
         }
 
         ffi::add_history(line_ptr as *const i8);
 
         let line = String::from_raw_buf(line_ptr as *const u8);
 
-        Some(line)
+        Ok(line)
     }
 }
 
 
 #[cfg(target_os = "windows")]
-pub fn readline(prompt: &str) -> Option<String> {
+pub fn readline(prompt: &str) -> IoResult<String> {
     use std::io::stdio::stdin;
-    use std::io::IoErrorKind;
     print!("{}", prompt);
     let line = stdin().read_line();
     match line {
         Ok(mut s) => {
             s.pop(); // takes the last \n off of the returned string
-            Some(s)
+            Ok(s)
         },
         Err(e) => {
-            if e.kind == IoErrorKind::EndOfFile {
-                return None;
-            } else {
-                panic!("IO Error! {}", e.desc);
-            }
+            Err(e)
         },
     }
 }
